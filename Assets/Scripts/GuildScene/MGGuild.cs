07c18cf646764a4da47d3431741c61105571045d
YiYop <wjdrbsdl1011@naver.com>
2025-02-23 22:09:00 +0900

    [추가] 길드씬 관리할 매니저 1. 길드 데이터 생성하고, 케릭 영입 해고 진행

---
+++ Assets/Scripts/GuildScene/MGGuild.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class MGGuild : SigleTon<MGGuild>
+{
+    public static GuildData GuildData;
+
+    // Use this for initialization
+    void Start()
+    {
+        GuildData guildData = new GuildData();
+        GuildData = guildData;
+    }
+
+    public void OnClickScout(CharactorData _scoutChar)
+    {
+        GuildData.Scout(_scoutChar);
+    }
+    public void OnClickFire(CharactorData _fireChar)
+    {
+        GuildData.Fire(_fireChar);
+    }
+}
