14ec53051ac66c7f192aa403c8844462bf51899d
YiYop <wjdrbsdl1011@naver.com>
2025-02-23 19:34:28 +0900

    [추가] 채용 UI 1. 영입하기 버튼 누르면, 임의로 정한 수량만큼 캐릭데이터 생성해서 표기

--- Assets/Scripts/GuildScene/UIRecruit.cs
+++ Assets/Scripts/GuildScene/UIRecruit.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIRecruit : MonoBehaviour
+public class UIRecruit : UIBase
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Window;
+    public CharSlot SlotSample;//슬롯 만들 샘플
+    public Transform grid;//만들 그리드
+
+    public CharSlot[] charSlots; //정보 표기할 슬롯들
+    public int tempRecruitCount = 3;
+    public void OnClickRecruit()
     {
-        
+        //영입하기 버튼을 누르면
+        //창을 연다
+        Window.SetActive(true);
+        //필요한 만큼 슬롯을 만드는 부분
+        MakeSamplePool(ref charSlots, SlotSample.gameObject, tempRecruitCount, grid);
+
+        SetCharInfo();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetCharInfo()
     {
-        
+        for (int i = 0; i < tempRecruitCount; i++)
+        {
+            //지원한 캐릭터 데이터 생성 - 임의로 3개, 임의로 옵션 동일
+            CharactorData charData = new CharactorData();
+            charSlots[i].SetInfo(charData);
+            charSlots[i].gameObject.SetActive(true);
+        }
+        OffRestSlot(ref charSlots, tempRecruitCount);
+
     }
 }
