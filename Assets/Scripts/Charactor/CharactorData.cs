83b10c74c5277dd8ca5556a48062147f2b5b77ef
YiYop <wjdrbsdl1011@naver.com>
2025-02-23 16:47:19 +0900

    [추가] 캐릭터 기본 데이터 클래스

---
+++ Assets/Scripts/Charactor/CharactorData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum EnumCharctorStat
+{
+    MaxHp, CurHp, AttackPower, DefencePower, MoveSpeed, AttackReach, AttackCoolTime, AttackCurCoolTime
+}
+
+public class CharactorData
+{
+    static private int MakeCount = 1;
+    public string Name;
+    public int[] Stats;
+    
+    public CharactorData()
+    {
+        Stats = new int[Enum.GetValues(typeof(EnumCharctorStat)).Length];
+        Name = "테스트"+MakeCount.ToString();
+        MakeCount++;
+    }
+}
