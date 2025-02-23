b6b1fc9db8fbd50b4cbae7d06339fe88078d1893
YiYop <wjdrbsdl1011@naver.com>
2025-02-23 22:09:48 +0900

    [추가] 길드 데이터 클래스 1. 보유 케릭이나 평판, 진행도등 길드 데이터 부분을 담을 그릇

---
+++ Assets/Scripts/GuildScene/GuildData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//길드 데이터 
+public class GuildData
+{
+    public List<CharactorData> m_haveCharList = new List<CharactorData>();
+    public int m_maxCharSpace = 5;
+
+    public bool Scout(CharactorData _char)
+    {
+        if (m_haveCharList.Count >= m_maxCharSpace)
+        {
+            //공간부족
+            return false;
+        }
+        m_haveCharList.Add(_char);
+        return true;
+    }
+
+    public void Fire(CharactorData _char)
+    {
+        int index = m_haveCharList.IndexOf(_char);
+        if ( index < 0)
+        {
+            //없는 케릭이면 리턴
+            return;
+        }
+        m_haveCharList.RemoveAt(index);
+    }
+
+    public List<CharactorData> GetCharList()
+    {
+        return m_haveCharList;
+    }
+}
