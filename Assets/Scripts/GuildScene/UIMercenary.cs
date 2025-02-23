36d37d79abcae9beb31280f0facba73ebaa32e0d
YiYop <wjdrbsdl1011@naver.com>
2025-02-23 22:10:13 +0900

    [수정] 보유 케릭 보여주기 1. 보유한 케릭들로 케릭슬롯들 세팅

--- Assets/Scripts/GuildScene/UIMercenary.cs
+++ Assets/Scripts/GuildScene/UIMercenary.cs
@@ -10,8 +10,10 @@ public class UIMercenary : UIBase
 
     public CharSlot[] charSlots; //정보 표기할 슬롯들
     public int tempRecruitCount = 3;
-    public void OnClickRecruit(List<CharactorData> _haveMercenaryList)
+
+    public void OnClickShowHaveChares()
     {
+        List<CharactorData> _haveMercenaryList = MGGuild.GuildData.GetCharList();
         //보유한 용병 데이터를 가지고 용병 정보창 생성 
         //창을 연다
         Window.SetActive(true);
@@ -19,16 +21,14 @@ public class UIMercenary : UIBase
         //필요한 만큼 슬롯을 만드는 부분
         MakeSamplePool(ref charSlots, SlotSample.gameObject, tempRecruitCount, grid);
 
-        SetCharInfo();
+        SetCharInfo(_haveMercenaryList);
     }
 
-    private void SetCharInfo()
+    private void SetCharInfo(List<CharactorData> _haveMercenaryList)
     {
-        for (int i = 0; i < tempRecruitCount; i++)
+        for (int i = 0; i < _haveMercenaryList.Count; i++)
         {
-            //지원한 캐릭터 데이터 생성 - 임의로 3개, 임의로 옵션 동일
-            CharactorData charData = new CharactorData();
-            charSlots[i].SetInfo(charData);
+            charSlots[i].SetInfo(_haveMercenaryList[i]);
             charSlots[i].gameObject.SetActive(true);
         }
         OffRestSlot(ref charSlots, tempRecruitCount);
