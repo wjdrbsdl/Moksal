2e6ed195764fe60fd67ff940791dfd8f570c37f5
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-26 17:37:57 +0900

    [수정] 캐릭스크립트 없애면서 에셋 변수 조정

--- Assets/Scripts/BattleScene/AssetManager.cs
+++ Assets/Scripts/BattleScene/AssetManager.cs
@@ -7,5 +7,5 @@ public class AssetManager : SigleTon<AssetManager>
 
     //에셋 매니저에 있어야할것들
     public CharactorBase m_monsterSample;
-    public CharPlayer m_playerSample;
+    public CharactorBase m_playerSample;
 }
