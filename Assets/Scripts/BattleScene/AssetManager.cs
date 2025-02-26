eed797cfd19b8858b153c58d8a66d61390ece0bd
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-26 17:41:06 +0900

    [추가] 배틀필드 Go 쓰던거 복구

--- Assets/Scripts/BattleScene/AssetManager.cs
+++ Assets/Scripts/BattleScene/AssetManager.cs
@@ -8,4 +8,5 @@ public class AssetManager : SigleTon<AssetManager>
     //에셋 매니저에 있어야할것들
     public CharactorBase m_monsterSample;
     public CharactorBase m_playerSample;
+    public GameObject m_fieldObj;
 }
