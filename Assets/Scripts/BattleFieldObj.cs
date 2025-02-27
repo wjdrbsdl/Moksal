55438c4344755d81d443426de54900b773a5c7c7
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-27 14:11:28 +0900

    [추가] 전장 obj 스크립트

---
+++ Assets/Scripts/BattleFieldObj.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleFieldObj : MonoBehaviour
+{
+    //전장을 표현한 Obj
+    public BattleFieldData fieldData; 
+
+}
