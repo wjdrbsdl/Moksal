866966c42586e19ed2b8d3d5fb5bcf1f62816be1
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-27 12:15:26 +0900

    [추가] 미션에 스폰할 몬스터, 출전할 용병 추가 함수 1. 미션 만들때 db에서 따올걸 임의로 데이터 생성해서 전장필드 생성 - 전장 위치, 스폰 몬스터, 플레이어 초기 스폰지역 여부 할당 2. 전장지역에 스폰데이터 추가 기능 - 플레이어가 전장에 추가로 스폰데이터를 넣기 위해서 구현

--- Assets/Scripts/BattleScene/MissionMaker.cs
+++ Assets/Scripts/BattleScene/MissionMaker.cs
@@ -16,11 +16,15 @@ public class MissionMaker : MonoBehaviour
     public float spawnRadius = 5f;
     public Transform[] m_targetPoints;
     public bool sceneMove = false;
+    public static Mission curMission;
+
     // Use this for initialization
     void Start()
     {
         Mission mission = MakeMission();
         curMission = mission;
+        AddPlayerChar(); //플레이어가 아군 용병을 넣는 부분
+        
         if(sceneMove == true)
         {
             Invoke(nameof(LoadBattleScene), 2f);
@@ -34,27 +38,52 @@ public class MissionMaker : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    public static Mission curMission;
+
 
     private Mission MakeMission()
     {
-        //타겟 포인트는 - 플레이어, 몬스터 스폰지로서 맵정보에 담겨 있어야할 것들. 임의로 월드 좌표에 설정해놨음
-        //파싱 데이터로 - 미션지에 몬스터 스폰지와, 몬스터 정보
-        //플레이어 조작으로 - 플레이어 스폰지에, 용병을 할당해서 정보를 만들것. 
-        Transform[] targetPoints = m_targetPoints;
-        Mission mission = new Mission();
-        
+        //파싱에서 - 기본적으로 전장지역(전장 위치, 스폰 오브젝트, 플레이어 시작여부)로 되어있어야함.
+        Transform[] targetPoints = m_targetPoints; //임의로 전장 위치 
+
+        Mission mission = new Mission(); //임의로 미션을 생성함 - 추후 db로 가져올 것. 
+
+        //필드 정보 담을 리스트
         List<BattleFieldData> battleFieldList = new List<BattleFieldData>();
-        BattleFieldData playerField = new BattleFieldData(targetPoints[0], 0, true);
+
+        //db 정의된 전장수만큼 필드 데이터 생성 
+        //플레이어가 소환될 수 있는 시작 전장지역 - true, 소환데이터는 - 추가로 할당해야함. 
+        BattleFieldData playerField = new BattleFieldData(targetPoints[0], 0, null, true);
         battleFieldList.Add(playerField);
         
+        //몬스터 소환될 전장 생성
         for (int i = 1; i < targetPoints.Length; i++)
         {
-            battleFieldList.Add(new BattleFieldData(targetPoints[i], i));
+
+            CharactorData[] spawnEnemys = new CharactorData[3];
+            for(int e = 0; e < spawnEnemys.Length; e++)
+            {
+                CharactorData enemyData = new CharactorData();
+                spawnEnemys[e] = enemyData;
+            }
+            
+            battleFieldList.Add(new BattleFieldData(targetPoints[i], i, spawnEnemys, false));
         }
         mission.battleFields = battleFieldList.ToArray();
         return mission;
     }
 
+    private void AddPlayerChar()
+    {
+        //투입가능한 전장에 플레이어 넣기 -> 일단 일괄적으로 플레이어 전장이라고 표기된 부분에 넣기
+        CharactorData[] spawnPlayers = new CharactorData[1];
+        for (int e = 0; e < spawnPlayers.Length; e++)
+        {
+            CharactorData playerData = new CharactorData();
+            spawnPlayers[e] = playerData;
+        }
+        curMission.inputPlayers = spawnPlayers;
+        curMission.GetBattleField(0).AddSpawnData(spawnPlayers);
+
+    }
 }
 
