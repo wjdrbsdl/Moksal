5c1dc0ad9accd46022ba4ab45f9ea76ce6c67327
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-27 12:24:38 +0900

    [추가] 캐릭오브젝트 행동방식 1. obj에 쿨다운 추가 2. 이동함수와 액션함수 구별 3. 액션로직에서 개별 조건 따져서 하려던 행동 수행 - 지금은 전투만

--- Assets/Scripts/Charactor/CharactorObj.cs
+++ Assets/Scripts/Charactor/CharactorObj.cs
@@ -10,18 +10,22 @@ public class CharactorObj : MonoBehaviour
     public BattleFieldData m_battleField; //현재 케릭터가 있는 전장
     public CharactorData m_charData;
 
-    private Transform target;
+    private float m_attackCool;
+    private float m_curCool;
+    private CharactorObj target;
     private Vector3 goal; //타겟없을때 목적지
     private bool isFowardGoal; //골로 향하는 중인가
 
     private void Update()
     {
+        m_curCool += Time.deltaTime; //현재쿨 갱신
         //타겟을 찾아본다.
         //못찾았다고 치자.
         FindEnemy();
         //목적지로 움직인다
         Move();
         //공격한다
+        DoAction();
 
     }
 
@@ -35,9 +39,12 @@ public class CharactorObj : MonoBehaviour
 
         for (int i = 0; i < m_battleField.charactorList.Count; i++)
         {
-             if(isMonster != m_battleField.charactorList[i].isMonster)
+            if (m_battleField.charactorList[i] == null)
+                continue;
+
+            if (isMonster != m_battleField.charactorList[i].isMonster)
             {
-                target = m_battleField.charactorList[i].gameObject.transform;
+                target = m_battleField.charactorList[i];
                 isFowardGoal = false;
                 break;
             }
@@ -48,9 +55,12 @@ public class CharactorObj : MonoBehaviour
     private void Move()
     {
         //타겟있으면 현재 타겟있는 위치를 목표 위치로
-        if(target != null)
+        //어느 선까지 이동할건지
+        float gap = 0.3f;
+        if (target != null)
         {
-            goal = target.position;
+            goal = target.gameObject.transform.position;
+            gap = m_charData.GetCharStat(EnumCharctorStat.AttackReach);
         }
         else if(isFowardGoal == false)
         {
@@ -64,24 +74,58 @@ public class CharactorObj : MonoBehaviour
             isFowardGoal = true;
         }
 
-        //목적지를 향해 이동 
+        //목적지 까지 도달했으면 이동 안함 
+        if(Vector3.Distance(goal, transform.position) <= gap)
+        {
+            return;
+        }
         transform.position += (goal - transform.position).normalized * Time.deltaTime * m_charData.GetCharStat(EnumCharctorStat.MoveSpeed);
         
-        if(Vector3.Distance(transform.position, goal) <= 0.3f)
+    
+    }
+
+    private void DoAction()
+    {
+        //이동 후 
+        float gap = 0.3f;
+        if (target != null)
         {
+            gap = m_charData.GetCharStat(EnumCharctorStat.AttackReach);
+        }
+
+        //목적지 까지 도달했으면 행동함 
+        if (Vector3.Distance(transform.position, goal) <= gap)
+        {
+            if (target != null)
+            {
+                //타겟 쫓아왔으면 공격
+                if(m_curCool >= m_attackCool)
+                {
+                    //공격하고
+                    target.Attack();
+                    m_curCool = 0;
+
+                    //사망체크 나중엔 딴 함수로
+                    if(target.gameObject == null)
+                    {
+                        target = null; //타겟 초기화 
+                    }
+                }
+                
+                return;
+            }
+
+            //그냥 목적지 까지 걸어간거면 목적지 초기화
             isFowardGoal = false;
         }
     }
 
     private void RestrictPos(ref Vector3 _goal)
     {
-
         _goal.x = Mathf.Min(m_battleField.fieldRight, _goal.x);
         _goal.x = Mathf.Max(m_battleField.fieldLeft, _goal.x);
         _goal.y = Mathf.Min(m_battleField.fieldUp, _goal.y);
         _goal.y = Mathf.Max(m_battleField.fieldDown, _goal.y);
-
-
     }
 
     public void SetBattleField(BattleFieldData _fieldData)
@@ -89,24 +133,24 @@ public class CharactorObj : MonoBehaviour
         m_battleField = _fieldData;
     }
 
-    public void SetFieldNumber(int _num)
-    {
-        m_fieldNumber = _num;
-    }
-
     public void SetCharactorData(CharactorData _charData)
     {
         m_charData = _charData;
         _charData.charObj = this;
+
+        m_attackCool = _charData.GetCharStat(EnumCharctorStat.AttackCoolTime);
+        m_curCool = 0;
     }
 
     public void Attack()
     {
+        Debug.Log("타겟 공격");
         m_charData.Attack(10);
     }
+
     public void Dead()
     {
-        BattleManager.Instance.ReportBattle(m_fieldNumber);
+        BattleManager.Instance.ReportBattle(m_battleField.fieldNumber);
         Destroy(gameObject);
     }
 
@@ -120,11 +164,4 @@ public class CharactorObj : MonoBehaviour
         target = null;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (isMonster == true)
-            return;
-
-        other.GetComponent<CharactorObj>().Attack();
-    }
 }
