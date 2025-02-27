141282c4c062ef59230644a92c72923e6374d3e1
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-27 15:42:53 +0900

    [추가] 전장에서 용병 표기할 아이콘 UI 세팅 1. 씬에서 obj 연결 2. UI에 세팅하면 슬롯을 만들고 obj를 연결

--- Assets/Scripts/BattleScene/PlayerCharIcon.cs
+++ Assets/Scripts/BattleScene/PlayerCharIcon.cs
@@ -6,6 +6,11 @@ public class PlayerCharIcon : MonoBehaviour
 {
     private CharactorObj m_charObj;
 
+    public void SetInfo(CharactorObj _charObj)
+    {
+        m_charObj = _charObj;
+    }
+
     public void OnClickIcon()
     {
         CameraFollows.SetCamTarget(m_charObj);
--- Assets/Scripts/BattleScene/UIPlayerCharIcon.cs
+++ Assets/Scripts/BattleScene/UIPlayerCharIcon.cs
@@ -8,10 +8,26 @@ public class UIPlayerCharIcon : UIBase
     public PlayerCharIcon SlotSample;//슬롯 만들 샘플
     public Transform grid;//정렬할 그리드
 
-    public PlayerCharIcon[] charSlots; //정보 표기할 슬롯들
+    public PlayerCharIcon[] m_charSlots; //정보 표기할 슬롯들
 
-    public void SetCharIcons()
+    private List<CharactorObj> m_charList;
+
+    public void SetCharIcons(List<CharactorObj> _playerList)
     {
-        
+        m_charList = _playerList;
+        MakeSamplePool(ref m_charSlots, SlotSample.gameObject, _playerList.Count, grid);
+
+        SetIconInfo();
     }
+
+    private void SetIconInfo()
+    {
+        for (int i = 0; i < m_charList.Count; i++)
+        {
+            m_charSlots[i].gameObject.SetActive(true);
+            m_charSlots[i].SetInfo(m_charList[i]);
+        }
+        OffRestSlot(ref m_charSlots, m_charList.Count);
+    }
+    
 }
