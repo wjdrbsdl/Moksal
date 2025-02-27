89b5c705e96ff00a6d39233242e66c97905f0b38
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-27 17:51:45 +0900

    [추가] 영입하기 누른경우 소개 리스트에서 제거

--- Assets/Scripts/GuildScene/CharSlot.cs
+++ Assets/Scripts/GuildScene/CharSlot.cs
@@ -13,6 +13,7 @@ public class CharSlot : MonoBehaviour
     public Text InfoText;
     public EnumCharSlotFunction function = EnumCharSlotFunction.Scout;
     private CharactorData curCharData;
+    private ICharSlotClickCallBack iClickCallBack;
 
     public void SetInfo(CharactorData _charData)
     {
@@ -27,11 +28,38 @@ public class CharSlot : MonoBehaviour
         InfoText.text = statInfo;
     }
 
+    public void SetInfo(CharactorData _charData, ICharSlotClickCallBack _clickInterface)
+    {
+        iClickCallBack = _clickInterface;
+        curCharData = _charData;
+        string statInfo = "";
+        statInfo = _charData.Name + "\n";
+        for (int i = 0; i < _charData.Stats.Length - 1; i++)
+        {
+            statInfo += (EnumCharctorStat)(i) + ":" + _charData.Stats[i].ToString() + "\n";
+        }
+        statInfo += (EnumCharctorStat)(_charData.Stats.Length - 1) + ":" + _charData.Stats[_charData.Stats.Length - 1].ToString();
+        InfoText.text = statInfo;
+    }
+
+    public void Renew()
+    {
+        string statInfo = "";
+        statInfo = curCharData.Name + "\n";
+        for (int i = 0; i < curCharData.Stats.Length - 1; i++)
+        {
+            statInfo += (EnumCharctorStat)(i) + ":" + curCharData.Stats[i].ToString() + "\n";
+        }
+        statInfo += (EnumCharctorStat)(curCharData.Stats.Length - 1) + ":" + curCharData.Stats[curCharData.Stats.Length - 1].ToString();
+        InfoText.text = statInfo;
+    }
+
     public void OnClickBtn()
     {
         if(function == EnumCharSlotFunction.Scout)
         {
             MGGuild.Instance.OnClickScout(curCharData);
+            iClickCallBack.OnClickCharSlot(this);
         }
         else if (function == EnumCharSlotFunction.Fire)
         {
--- Assets/Scripts/GuildScene/UIRecruit.cs
+++ Assets/Scripts/GuildScene/UIRecruit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIRecruit : UIBase
+public class UIRecruit : UIBase, ICharSlotClickCallBack
 {
     public GameObject Window;
     public CharSlot SlotSample;//슬롯 만들 샘플
@@ -10,6 +10,30 @@ public class UIRecruit : UIBase
 
     public CharSlot[] charSlots; //정보 표기할 슬롯들
     public int tempRecruitCount = 3;
+
+    public void OnClickCharSlot(CharSlot _charSlot)
+    {
+        int index = -1;
+        for(int i = 0; i< charSlots.Length; i++)
+        {
+            if(_charSlot == charSlots[i])
+            {
+                index = i;
+                break;
+            }
+        }
+        for(int i = index; i < charSlots.Length; i++)
+        {
+            charSlots[i] = null;
+            if(i+1 < charSlots.Length)
+            {
+                charSlots[i] = charSlots[i + 1];
+            }
+        }
+        RenewCharSlot();
+    }
+
+
     public void OnClickRecruit()
     {
         Window.SetActive(!Window.activeSelf);
@@ -23,13 +47,29 @@ public class UIRecruit : UIBase
         SetCharInfo();
     }
 
+    public void RenewCharSlot()
+    {
+        int infoCount = 0;
+        for (int i = 0; i < charSlots.Length; i++)
+        {
+            if (charSlots[i] == null)
+            {
+                break;
+            }
+            charSlots[i].Renew();
+            charSlots[i].gameObject.SetActive(true);
+            infoCount++;
+        }
+        OffRestSlot(ref charSlots, infoCount);
+    }
+
     private void SetCharInfo()
     {
         for (int i = 0; i < tempRecruitCount; i++)
         {
             //지원한 캐릭터 데이터 생성 - 임의로 3개, 임의로 옵션 동일
             CharactorData charData = new CharactorData(true);
-            charSlots[i].SetInfo(charData);
+            charSlots[i].SetInfo(charData, this);
             charSlots[i].gameObject.SetActive(true);
         }
         OffRestSlot(ref charSlots, tempRecruitCount);
