e2d2d374ff292ff5170ccb7f9c9c9648d1a5f148
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-27 12:21:29 +0900

    [수정] 전장지역 생성 구조 1. 몬스터, 캐릭터 생성 구조 일관화 - - 몬스터 여부는 캐릭데이터를 만들때 정의 - 동일 오브젝트 사용, - 내부 케릭 데이터는 미션에서 가져옴 - 전장에 있는 obj와 최초 스폰 시킬 데이터는 별개

--- Assets/Scripts/BattleScene/BattleFieldData.cs
+++ Assets/Scripts/BattleScene/BattleFieldData.cs
@@ -29,76 +29,90 @@ public class BattleFieldData
 
     public float spawnRadius ;
 
-    public bool isPlayer = false;
-    public int m_spawnMonsterCount;
+    public bool ablePlayerSpawn = false;
     public int m_restMonsterCount;
 
-    public List<CharactorBase> charactorList;
+    public CharactorData[] m_spawnCharData; //스폰시킬 케릭 데이터들
+    public List<CharactorObj> charactorList; //현재 필드에 있는 케릭터 Obj
 
     //전투가 일어나는 영역
     //배틀매니저는 각 필드를 돌아보며 전시상황을 살펴보고 
     //해당 필드를 기점으로 몬스터 스폰이나 전투 안내등을 진행 
     //
 
-    public BattleFieldData(Transform _fieldPos, int _fieldNum, bool _isPlayer = false)
+    public BattleFieldData(Transform _fieldPos, int _fieldNum, CharactorData[] _spawnObjects, bool _ablePlayerSpawn = false)
     {
         fieldNumber = _fieldNum;
+        //공간 설정
         width = 5f;
         height = 3f;
         pos = _fieldPos.position;
         float fieldHalfWidth = width / 2;
         float fieldHalfHeight = height / 2;
-   
         fieldLeft = pos.x - fieldHalfWidth;
         fieldRight = pos.x + fieldHalfWidth;
         fieldUp = pos.y + fieldHalfHeight;
         fieldDown = pos.y - fieldHalfHeight;
-        isPlayer = _isPlayer;
-        if (isPlayer == false)
+
+        //플레이어 스폰지역인가 -> 필요없음. 넣는 캐릭터 정보로 판단
+        ablePlayerSpawn = _ablePlayerSpawn;
+
+        m_spawnCharData = _spawnObjects;
+        if(m_spawnCharData == null)
         {
-            m_spawnMonsterCount = 1;
-            m_restMonsterCount = m_spawnMonsterCount;
+            //스폰 데이터 들어온게 없으면 크기 0 짜리로라도 만들어놓기. 
+            m_spawnCharData = new CharactorData[] { }; 
         }
-            
     }
 
     public void GenerateField()
     {
         GameObject fieldObj = MonoBehaviour.Instantiate(AssetManager.Instance.m_fieldObj, pos, Quaternion.identity);
         fieldObj.transform.localScale = new Vector3(width, height, 1);
-        string path = Application.dataPath + "/New Character Data2.json";
+        
         //정보들을 가지고 영역 생성
         //생성 갯수, 만들 샘플, 만들 위치, 구 범위로 만들어서 y 값만 0으로 조정 
-        int spawnCount = m_spawnMonsterCount;
+        int spawnCount = m_spawnCharData.Length;
         GameObject objectSample = AssetManager.Instance.m_monsterSample.gameObject; //생성될 몬스터는 추후 asset 관리자에서 가져올것. 
-        bool isMonster = true;
-        if (isPlayer)
+    
+        charactorList = new();
+        for (int i = 0; i < spawnCount; i++)
         {
-            spawnCount = 1;
-            isMonster = false;
-            objectSample = AssetManager.Instance.m_playerSample.gameObject;
-            path = Application.dataPath + "/F";
-        }
-        List<CharactorBase> spawnList = new();
-       for (int i = 0; i < spawnCount; i++)
-        {
-            //캐릭터 Go 생성
+            //스폰하려는 캐릭터 데이터
+            CharactorData charData = m_spawnCharData[i];
+            bool isPlayer = charData.isPlayer;
+            string path = Application.dataPath + "/New Character Data2.json";
+            if(charData.isPlayer)
+                path = Application.dataPath + "/F";
+
+            //스폰할 위치 계산
             Vector3 spawnPos = Random.insideUnitSphere * spawnRadius + pos;
             spawnPos.z = 0;
-            CharactorBase charactor = MonoBehaviour.Instantiate(objectSample, spawnPos, Quaternion.identity).GetComponent<CharactorBase>();
-            spawnList.Add(charactor);
-            charactor.isMonster = isMonster;
-            //캐릭터에 정보 전달
-            charactor.SetFieldNumber(fieldNumber);
-            charactor.SetBattleField(this); //전장 정보 넘기고 
 
+            //캐릭 오브젝트 생성
+            CharactorObj charactorObj = MonoBehaviour.Instantiate(objectSample, spawnPos, Quaternion.identity).GetComponent<CharactorObj>();
+            charactorList.Add(charactorObj);
+            charactorObj.isMonster = !isPlayer; 
+
+            //캐릭 오브젝트에 값 할당
+            charactorObj.SetBattleField(this); //전장 정보
+            charactorObj.SetCharactorData(charData); //오브젝트에 캐릭데이터 할당
+
+            //캐릭터 데이터에 obj 연결
+            charData.SetCharObj(charactorObj);
 
             //캐릭터 껍데기 
             CharacterViewer viewer = objectSample.GetComponentInChildren<CharacterViewer>();
             viewer.gameObject.transform.localScale = new Vector3(0.25f, 0.25f, 1);
             viewer.LoadFromJSON(path);
+
+            if (!isPlayer)
+            {
+                RestMonsterCount++;
+            }
+                
         }
-        charactorList = spawnList;
+      
     }
 
     public void KillMonster()
@@ -112,10 +126,29 @@ public class BattleFieldData
         {
             return m_restMonsterCount;
         }
+        set
+        {
+            m_restMonsterCount = value;
+        }
     }
 
-    public List<CharactorBase> GetCharList()
+    public List<CharactorObj> GetCharList()
     {
         return charactorList;
+    }
+
+    public void AddSpawnData(CharactorData[] _addCharData)
+    {
+        //스폰시킬 데이터 정보를 넣으면 
+        CharactorData[] renewSpawnData = new CharactorData[m_spawnCharData.Length + _addCharData.Length];
+        for (int i = 0; i < m_spawnCharData.Length; i++)
+        {
+            renewSpawnData[i] = m_spawnCharData[i];
+        }
+        for (int i = 0; i < _addCharData.Length; i++)
+        {
+            renewSpawnData[i + m_spawnCharData.Length] = _addCharData[i];
+        }
+        m_spawnCharData = renewSpawnData;
     }
 }
