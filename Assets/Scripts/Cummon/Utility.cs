50266cdeb213cedf4046efaf69df8c74923f0504
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-27 16:47:03 +0900

    [추가] int 100을 float로 전환할 공용함수 정의

---
+++ Assets/Scripts/Cummon/Utility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class Utility
+{
+    public static float CalHundred(int _intValue)
+    {
+        //100분률 된 인트값을 float 로
+        return (float)(_intValue * 0.01f);
+    }
+}
+
