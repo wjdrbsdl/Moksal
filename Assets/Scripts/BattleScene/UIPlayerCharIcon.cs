58cdf22891f9cdc6c77b0e746e77a013025ef1d4
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-27 15:27:13 +0900

    [추가] 배틀씬에서 참여 용병 표기할 UI

---
+++ Assets/Scripts/BattleScene/UIPlayerCharIcon.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPlayerCharIcon : UIBase
+{
+    public GameObject Window;
+    public PlayerCharIcon SlotSample;//슬롯 만들 샘플
+    public Transform grid;//정렬할 그리드
+
+    public PlayerCharIcon[] charSlots; //정보 표기할 슬롯들
+
+    public void SetCharIcons()
+    {
+
+    }
+}
