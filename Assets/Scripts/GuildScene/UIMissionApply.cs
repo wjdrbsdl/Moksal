191ca274f14a3b0e69f999c88c86c257b506b790
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-28 11:05:15 +0900

    [추가] 미션 신청에서 투입, 대기 슬롯 아이콘 클릭

--- Assets/Scripts/GuildScene/UIMissionApply.cs
+++ Assets/Scripts/GuildScene/UIMissionApply.cs
@@ -11,23 +11,39 @@ public class UIMissionApply : UIBase
     public Transform m_haveGrid;//만들 그리드
 
     public IconMissionChar[] m_inputCharIcons; //정보 표기할 슬롯들
-    public IconMissionChar[] m_haveCharIcons; //정보 표기할 슬롯들
+    public IconMissionChar[] m_waitCharIcons; //정보 표기할 슬롯들
 
+    [SerializeField]
+    private List<CharactorData> m_inputList;
+    [SerializeField]
+    private List<CharactorData> m_waitList;
+    private int ableInputCount; //투입가능수
     public void OpenApply(Mission _mission)
     {
+        //신청 창 온오프
         Window.SetActive(!Window.activeSelf);
 
         if (Window.activeSelf == false)
             return;
 
+        //신청 정보 표기 초기화
         int inputCount = _mission.inputPlayerCount;
         MakeSamplePool(ref m_inputCharIcons, SlotSample.gameObject, inputCount, m_inputGrid);
-
         List<CharactorData> haveCharList = MGGuild.Instance.GetHaveCharList();
-        MakeSamplePool(ref m_haveCharIcons, SlotSample.gameObject, haveCharList.Count, m_haveGrid);
-
+    
+        MakeSamplePool(ref m_waitCharIcons, SlotSample.gameObject, haveCharList.Count, m_haveGrid);
         InputOff(); //투입 용병은 다 꺼놓기 
         SetHaveChars(haveCharList);
+
+        //신청 위한 데이터 초기화
+        m_inputList = new();
+        m_waitList = new();
+        for (int i = 0; i < haveCharList.Count; i++)
+        {
+            // 데이터 복사 
+            m_waitList.Add(haveCharList[i]);
+        }
+        ableInputCount = _mission.inputPlayerCount;
     }
 
     private void InputOff()
@@ -43,14 +59,67 @@ public class UIMissionApply : UIBase
     {
         for (int i = 0; i < _haveCharList.Count; i++)
         {
-            m_haveCharIcons[i].gameObject.SetActive(true);
-            m_haveCharIcons[i].SetIcon(_haveCharList[i], EMissionBtnType.Have);
+            m_waitCharIcons[i].gameObject.SetActive(true);
+            m_waitCharIcons[i].SetIcon(_haveCharList[i], EMissionBtnType.Wait);
         }
-        OffRestSlot(ref m_haveCharIcons, _haveCharList.Count);
+        OffRestSlot(ref m_waitCharIcons, _haveCharList.Count);
     }
 
     public void SelectChar(EMissionBtnType _btnType, CharactorData _charData)
     {
+        if(_btnType == EMissionBtnType.Input)
+        {
+         
+            TakeOffChar(_charData);
+        }
+        else if(_btnType == EMissionBtnType.Wait)
+        {
+         
+            InputChar(_charData);
+            
+        }
 
+        //뭐든 했으면 슬롯은 다시 갱신
+        RenewSlots();
     }
+
+    private void TakeOffChar(CharactorData _charData)
+    {
+        //투입에서 누른거면
+        //투입 리스트에서 빼고
+        //대기 리스트에 추가 
+        m_inputList.Remove(_charData);
+        m_waitList.Add(_charData);
+    }
+
+    private void InputChar(CharactorData _charData)
+    {  
+        //대기에서 누른거면
+        //투입 리스트 수량 확인해서 투입가능하면 
+        //대기 리스트에서 빼고
+        //투기 리스트에 추가 
+        if (m_inputList.Count == ableInputCount)
+            return;
+
+        m_inputList.Add(_charData);
+        m_waitList.Remove(_charData);
+    }
+
+    private void RenewSlots()
+    {
+        for (int i = 0; i < m_inputList.Count; i++)
+        {
+            m_inputCharIcons[i].gameObject.SetActive(true);
+            m_inputCharIcons[i].InputChar(m_inputList[i]);
+        }
+        OffRestSlot(ref m_inputCharIcons, m_inputList.Count);
+
+        for (int i = 0; i < m_waitList.Count; i++)
+        {
+            m_waitCharIcons[i].gameObject.SetActive(true);
+            m_waitCharIcons[i].InputChar(m_waitList[i]);
+        }
+        OffRestSlot(ref m_waitCharIcons, m_waitList.Count);
+    }
+
 }
