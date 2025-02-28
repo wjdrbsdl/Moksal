5d06bea740af7c8b86ece22234b39187a0daeb72
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-28 11:00:19 +0900

    [추가] 아이콘 세팅할때 케릭 데이터 할당

--- Assets/Scripts/GuildScene/IconMissionChar.cs
+++ Assets/Scripts/GuildScene/IconMissionChar.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public enum EMissionBtnType
 {
-    Input, Have
+    Input, Wait
 }
 
 public class IconMissionChar : MonoBehaviour
@@ -14,20 +14,26 @@ public class IconMissionChar : MonoBehaviour
     public Image m_charIcon;
     public EMissionBtnType m_btnType = EMissionBtnType.Input;
     public UIMissionApply m_missionApply;
+    public CharactorData m_charData;
 
     public void SetIcon(CharactorData _charData, EMissionBtnType _btnType)
     {
-        m_btnType = _btnType;
+        m_charData = _charData;
+       m_btnType = _btnType;
     }
     public void SetIcon(EMissionBtnType _btnType)
     {
         m_btnType = _btnType;
     }
+    public void InputChar(CharactorData _charData)
+    {
+        m_charData = _charData;
+    }
 
 
     public void OnClickIcon()
     {
-
+        m_missionApply.SelectChar(m_btnType, m_charData);
     }
 }
 
