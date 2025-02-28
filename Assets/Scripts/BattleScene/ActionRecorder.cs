2632bab3f49a17f3e9673e2fa9e6cf99a93aeecd
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-28 15:08:48 +0900

    [추가] 행동 기록 클래스 1. 어택 구조체 생성

---
+++ Assets/Scripts/BattleScene/ActionRecorder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ActionRecorder
+{
+
+
+}
+
+public struct AttackRecorde
+{
+    public string attackerName;
+    public string targetName;
+    public int inputDamage;
+    public int adaptDamage;
+
+    public AttackRecorde(string _attacker, string _target, int _damage)
+    {
+        attackerName = _attacker;
+        targetName = _target;
+        inputDamage = adaptDamage = _damage;
+    }
+}
+
