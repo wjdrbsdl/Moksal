57e9fbeb680c97ab0971dc3f5c3a9d79998c1b1c
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-28 10:16:07 +0900

    [추가] 미션에 투입가능한 용병수

--- Assets/Scripts/BattleScene/Mission.cs
+++ Assets/Scripts/BattleScene/Mission.cs
@@ -6,8 +6,13 @@ public class Mission
     public BattleFieldData[] battleFields; //위치, 너비, 소환할 몬스터, 소환할 수 
     public CharactorData[] inputPlayers; //투입될 용병들
     public int finalGoal; //이것도 마지막 배틀필드로?
-    public int players; //참여 용병들
+    public int inputPlayerCount; //참여 용병들
 
+    public Mission()
+    {
+        Random random = new();
+        inputPlayerCount = random.Next() % 6 + 1;
+    }
 
     public BattleFieldData GetBattleField(int _index)
     {
