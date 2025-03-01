b90e05c4ebc70f779aaa6a34e3e10f5b6383073f
YiYop <wjdrbsdl1011@naver.com>
2025-03-01 20:40:28 +0900

    [추가] 캐릭 행동 별개 조정 1. CharObj에서 진행할 행동 선택 2. ActionBase에서 각 행동에 쓰일 공통변수와, 쿨타임 초기함수 정의 3. 케릭 인터페이스로 ? 어차피 상속햇는데 할이유가 있나 싶네

---
+++ Assets/Scripts/Charactor/CharActionBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class CharActionBase
+{
+    protected float m_actionDelayTime = 1f; //해당 공격이후 지체시간
+    protected float m_actionCoolTime = 1f; //공격 주기
+    protected float m_culCoolTime = 1f;  //현재 지난 주기
+    protected DateTime m_preTime; //이전 행동 설정했던 시간
+
+    public void CalDeltaTime()
+    {
+        //해당 행동이 시작되면, 이전 쿨타임을 계산
+        TimeSpan span = DateTime.Now - m_preTime;
+        m_culCoolTime -= (float)span.TotalMilliseconds;
+    }
+}
--- Assets/Scripts/Charactor/CharactorObj.cs
+++ Assets/Scripts/Charactor/CharactorObj.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 
 
+public enum EnumAniState
+{
+    Idle, Move, Attack
+}
 public class CharactorObj : MonoBehaviour
 {
     #region 고유 데이터
@@ -11,23 +15,18 @@ public class CharactorObj : MonoBehaviour
     #region 플레이어 행동 데이터
     public BattleFieldData m_battleField; //현재 케릭터가 있는 전장
     private Animator m_animator;
-    private float m_actionSpeed; //공격 속도 - 공격 프레임이 끝나는 주기? -> 하는 행동에 따라 값을 받아올것. 
-    private float m_curWaitTime = 0; //동작 대기 시간 
+    public float m_actionSpeed; //공격 속도 - 공격 프레임이 끝나는 주기? -> 하는 행동에 따라 값을 받아올것. 
+    public float m_curWaitTime = 0; //동작 대기 시간 
+    
+    public float m_actionCool;
+    public float m_curCool;
 
-    private float m_actionCool;
-    private float m_curCool;
-
-    private CharactorObj target;
-    private Vector3 goal; //타겟없을때 목적지
-    private bool isFowardGoal; //골로 향하는 중인가
-    private EnumAniState m_aniState;
+    public ICharAction[] haveActions = {new AttackAction(), new WanderAction()}; //보유한 행동
     #endregion
 
-    enum EnumAniState
-    {
-        Idle, Move, Attack
-    }
+  
 
+    private ICharAction curAction;
     private void Update()
     {
         m_curCool += Time.deltaTime; //현재쿨 갱신
@@ -35,16 +34,13 @@ public class CharactorObj : MonoBehaviour
         {
             return;
         }
-        //타겟을 찾아본다.
-        //못찾았다고 치자.
-        FindEnemy();
-        //목적지로 움직인다
-        Move();
-        //공격한다
-        DoAction();
-        //애니메이션
-    
-
+        if(curAction == null)
+        {
+            int randomAction = Random.Range(0, 2);
+            curAction = haveActions[randomAction];
+            curAction.CalDeltaTime();
+        }
+        curAction.Update(this);
     }
 
     private bool AbleActionState()
@@ -59,142 +55,34 @@ public class CharactorObj : MonoBehaviour
         return false;
     }
 
-    private void FindEnemy()
-    {
-        if (target != null)
-            return;
-
-        if (isMonster)
-            return;
-
-        float minDistance = float.MaxValue;
-        for (int i = 0; i < m_battleField.charactorList.Count; i++)
-        {
-            if (m_battleField.charactorList[i] == null)
-                continue;
-
-            if (isMonster != m_battleField.charactorList[i].isMonster)
-            {
-                float distance = Vector3.Distance(m_battleField.charactorList[i].transform.position, transform.position);
-                if(distance <= minDistance)
-                {
-                    minDistance = distance;
-                    target = m_battleField.charactorList[i];
-             
-                }
-                
-            }
-            if(target != null)
-            {
-                isFowardGoal = false;
-            }
-
-        }
-    }
-
-    private void Move()
+    public bool Move(Vector3 _goal, float _paddingDistance)
     {
         //타겟있으면 현재 타겟있는 위치를 목표 위치로
         //어느 선까지 이동할건지
-        float gap = 0.3f;
-        if (target != null)
-        {
-            goal = target.gameObject.transform.position;
-            gap = GetCharStat(EnumCharctorStat.AttackReach);
-        }
-        else if(isFowardGoal == false)
-        {
-            //타겟 없는 상황에 정해둔 목적지도 없으면
-            Vector3 curPos = transform.position;
-            float x = Random.Range(curPos.x - 5, curPos.x + 5);
-            float y = Random.Range(curPos.y - 5, curPos.y + 5);
-            //임의로 목적지 설정
-            goal = new Vector3(x, y, 0);
-            RestrictPos(ref goal);
-            isFowardGoal = true;
-        }
-
+        float gap = _paddingDistance;
+     
         //목적지 까지 도달했으면 이동 안함 
-        if(Vector3.Distance(goal, transform.position) <= gap)
+        if(Vector3.Distance(_goal, transform.position) <= gap)
         {
-            return;
+            return true;
         }
 
-        m_aniState = EnumAniState.Move;
-        PlayAnim();
-        transform.position += (goal - transform.position).normalized * Time.deltaTime * Utility.CalHundred(GetCharStat(EnumCharctorStat.MoveSpeed));
-        
-    
+        PlayAnim(EnumAniState.Move, _goal);
+        transform.position += (_goal - transform.position).normalized * Time.deltaTime * Utility.CalHundred(GetCharStat(EnumCharctorStat.MoveSpeed));
+        return false;
     }
 
-    private void DoAction()
-    {
-        //이동 후 
-        float gap = 0.3f;
-        if (target != null)
-        {
-            gap = GetCharStat(EnumCharctorStat.AttackReach);
-        }
-
-        //목적지 까지 도달했으면 행동함 
-        if (Vector3.Distance(transform.position, goal) <= gap)
-        {
-            if (target != null)
-            {
-                //타겟 쫓아왔으면 공격
-                if(m_curCool >= m_actionCool)
-                {
-                    if (target.IsDead())
-                    {
-                        //이미 사망한 상태라면 공격 안함
-                        target = null;
-                        return;
-                    }
-
-                        //공격하고
-                    m_aniState = EnumAniState.Attack;
-                    PlayAnim();
-                    ActionData attackRecorde = new ActionData(m_charData, target.m_charData, GetCharStat(EnumCharctorStat.AttackPower));
-                    target.Attack(attackRecorde);
-                    m_curCool = 0;
-                    m_curWaitTime = m_actionSpeed;
-
-                    //공격이 들어간순간 - gameObject 파괴 속도에 따라 타겟 널이 갈릴 수있음.
-                    //널인건 일단 죽였다는거, 여길왔다는건 얘가 때린거기때문에 보상 진행
-                    if(target==null || target.IsDead())
-                    {
-                        Debug.Log("상대 사망");
-                        m_charData.CalStat(EnumCharctorStat.MoveSpeed, 3);
-                        m_charData.CalStat(EnumCharctorStat.AttackPower, 3);
-                        m_charData.AccelerateActionSpeed(-2);
-                        m_actionSpeed = Utility.CalHundred(GetCharStat(EnumCharctorStat.ActionSpeed));
-                        m_charData.AccelerateCoolTime(-2);
-                        m_actionCool = Utility.CalHundred(GetCharStat(EnumCharctorStat.AttackCoolTime));
-                     
-                    }
-
-                }
-                
-                return;
-            }
-
-            //그냥 목적지 까지 걸어간거면 목적지 초기화
-            isFowardGoal = false;
-            m_aniState = EnumAniState.Idle;
-        }
-    }
-
-    private void PlayAnim()
+    public void PlayAnim(EnumAniState _aniEnum, Vector3 _goal)
     {
         int look = 1;
-        if((goal.x - transform.position.x) < 0)
+        if((_goal.x - transform.position.x) < 0)
         {
             //목적지가 오른쪽에있으면
             look = -1;
         }
-        transform.localScale = new Vector3(look, 1, 1);
+        
 
-        switch (m_aniState)
+        switch (_aniEnum)
         {
             case EnumAniState.Move:
                 m_animator.SetBool("Move", true);
@@ -204,12 +92,36 @@ public class CharactorObj : MonoBehaviour
                 m_animator.SetBool("Move", false);
                 m_animator.SetTrigger("AttackTrigger");
                 break;
+            case EnumAniState.Idle:
+                m_animator.SetBool("Move", false);
+                return;
         }
 
-        
+        transform.localScale = new Vector3(look, 1, 1);
+
+
     }
 
-    private void RestrictPos(ref Vector3 _goal)
+    public void ResetAction()
+    {
+        //각 파트에서 액션을 초기화할때 호출 
+        curAction = null;
+        PlayAnim(EnumAniState.Idle, transform.position);
+    }
+
+    public void ResetCool()
+    {
+        m_curCool = 0;
+    }
+
+    public void SetDelayTime(float _delayTime)
+    {
+        //특정 행동후 대기시간 설정
+        m_curWaitTime = _delayTime;
+    }
+
+    #region 공용
+    public void RestrictPos(ref Vector3 _goal)
     {
         _goal.x = Mathf.Min(m_battleField.fieldRight, _goal.x);
         _goal.x = Mathf.Max(m_battleField.fieldLeft, _goal.x);
@@ -227,15 +139,13 @@ public class CharactorObj : MonoBehaviour
         m_charData = _charData;
         _charData.SetCharObj(this);
 
-        m_aniState = EnumAniState.Idle;
         m_animator = GetComponentInChildren<Animator>();
 
         m_actionCool = Utility.CalHundred(GetCharStat(EnumCharctorStat.AttackCoolTime));
-        m_curCool = 0;
+        ResetCool();
         m_actionSpeed = Utility.CalHundred(GetCharStat(EnumCharctorStat.ActionSpeed));
     }
 
- 
     public int GetCharStat(EnumCharctorStat _charStat)
     {
         return m_charData.GetCharStat(_charStat);
@@ -280,8 +190,7 @@ public class CharactorObj : MonoBehaviour
         transform.position = movePos; //다음 전장 시작 위치로 순간이동 하고
         m_battleField = _nextField; //캐릭터에 필드 할당
         m_battleField.charactorList.Add(this);//필드에 캐릭터 할당 
-        isFowardGoal = false; //목적지 갱신
-        target = null;
     }
+    #endregion
 
 }
