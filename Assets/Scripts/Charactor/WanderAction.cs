4bededbaf33e44c06fbffebd7b3322a5cc133029
YiYop <wjdrbsdl1011@naver.com>
2025-03-01 20:41:10 +0900

    [추가] 방랑 액션 정의 1. 목표 없으면 목표정해서 도달할때까지 이동 2. 목표 도달하면 리셋하고 행동 리셋

---
+++ Assets/Scripts/Charactor/WanderAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WanderAction : CharActionBase, ICharAction
+{
+    bool isFowardGoal = false;
+    Vector3 goal;
+
+    public WanderAction()
+    {
+        m_actionDelayTime = 1f;
+    }
+
+    public void Update(CharactorObj _charObj)
+    {
+        if (isFowardGoal == false)
+        {
+            //타겟 없는 상황에 정해둔 목적지도 없으면
+            Vector3 curPos = _charObj.transform.position;
+            float x = Random.Range(curPos.x - 5, curPos.x + 5);
+            float y = Random.Range(curPos.y - 5, curPos.y + 5);
+            //임의로 목적지 설정
+            goal = new Vector3(x, y, 0);
+            _charObj.RestrictPos(ref goal);
+            isFowardGoal = true;
+        }
+        bool arrive = _charObj.Move(goal, 0.3f);
+        if (arrive == true)
+        {
+            isFowardGoal = false;
+            _charObj.ResetAction();
+            _charObj.SetDelayTime(m_actionDelayTime);
+        }
+            
+    }
+}
