4eee0ffa203a26aa9c3a7edc8aaa0ce86ac94587
YiYop <wjdrbsdl1011@naver.com>
2025-03-03 16:06:08 +0900

    [추가] 캐릭대사 매니저 1. 캐릭이 대사를 요구하면 대사 오브젝트를 만들어서 반환 - 대사 오브젝트는 canvas에 위치해야함

---
+++ Assets/Scripts/Charactor/CharDialogManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class CharDialogManager : SigleTon<CharDialogManager>
+{
+    public CharDialog dialogSample;
+    public Transform dialogBox;
+
+    public CharDialog GetDialogObj()
+    {
+        return Instantiate(dialogSample, dialogBox);
+    }
+}
