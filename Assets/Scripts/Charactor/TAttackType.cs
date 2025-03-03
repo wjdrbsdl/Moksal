263baad259b02938e490a6a46f591085bb3d9ba3
YiYop <wjdrbsdl1011@naver.com>
2025-03-03 18:30:32 +0900

    [추가] 공격 유형에 따른 데이터

---
+++ Assets/Scripts/Charactor/TAttackType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public struct TAttackType
+{
+    public int AttackPower;
+    public int AttackCoolTime;
+    public int AttackDelayTime;
+    public int AttackReach;
+
+    public TAttackType(int _power, int _coolTime, int _delay, int _reach)
+    {
+        AttackPower = _power;
+        AttackCoolTime = _coolTime;
+        AttackDelayTime = _delay;
+        AttackReach = _reach;
+    }
+}
